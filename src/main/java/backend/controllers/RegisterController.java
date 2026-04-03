ba826aa80bbedd5fa7ef4ca42e0a08632d403916
Surya-Premkumar <Surya.premkumar@city.ac.uk>
2026-04-03 13:26:32 +0100

    Update 13:25 03/04
    Added front End to Registration
    Still need to hook up registration to login page
    Fixed Basket button on basket

--- src/main/java/backend/controllers/RegisterController.java
+++ src/main/java/backend/controllers/RegisterController.java
@@ -1,19 +1,26 @@
 package backend.controllers;
 
 import backend.DatabaseManager;
+import backend.Main;
 import backend.communication.EmailSendResult;
 import backend.communication.SendGmail;
 import backend.interfaces.IApplicationAPI;
 import backend.models.Member;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Objects;
 import java.util.Random;
 
 public class RegisterController {
@@ -109,5 +116,30 @@ public class RegisterController {
         }
         System.out.println("invalid");
         return false;
+    }
+
+    @FXML public void goToCatalogue(ActionEvent event) {
+        switchPage(event, "Catalogue.fxml");
+    }
+    @FXML public void goToCurrentPromotions(ActionEvent event) {
+        switchPage(event, "CurrentPromotions.fxml");
+    }
+    @FXML public void goToBasket(ActionEvent event) {switchPage(event, "Basket.fxml");}
+    @FXML public void goToLogin(ActionEvent event) {switchPage(event, "Login.fxml");}
+
+
+    private void switchPage(ActionEvent event, String fxmlFile) {
+        try {
+            Parent root = FXMLLoader.load(
+                    Objects.requireNonNull(getClass().getResource("/frontend/" + fxmlFile))
+            );
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.getScene().setRoot(root);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
